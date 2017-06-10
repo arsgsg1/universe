ef140592a0d7ad646204ab6d392bea89966be6fb
yun <arsgsg1@gmail.com>
2017-06-10 20:07:31 +0900

    1012번 : 유기농 배추

--- baekjoon/1012.cpp
+++ baekjoon/1012.cpp
@@ -1,43 +1,44 @@
-#include <iostream>
-#include <string.h>
-#include <queue>
-using namespace std;
+#include <stdio.h>
 
-int arr[100001];	//입력은 1보다 크거나 같고, 10^6 == 100000와 같으므로 배열의 길이도 맞춰서 적용시켜 줍니다.
+int arr[50][50] = { 0 }, n, m;
+//참고 : flood_fill알고리즘
+//각각의 배추 묶음에 대해 카운트 한 후, 방문 표시를 한다.
+void flood_fill(int x, int y)
+{
+	arr[x][y] = 0;
+	if (x - 1 >= 0 && arr[x - 1][y] == 1)
+		flood_fill(x - 1, y);
+	if (x + 1<n && arr[x + 1][y] == 1)
+		flood_fill(x + 1, y);
+	if (y - 1 >= 0 && arr[x][y - 1] == 1)
+		flood_fill(x, y - 1);
+	if (y + 1<m&&arr[x][y + 1] == 1)
+		flood_fill(x, y + 1);
+}
 
 int main()
 {
-	int num;
-	queue<int> q;
-	cin >> num;
+	int t, num, x, y, count;
+	scanf("%d", &t);
+	for (int z = 0; z < t; ++z)
+	{
+		count = 0;
+		scanf("%d %d %d", &m, &n, &num);
+		for (int i = 0; i < num; ++i)
+		{
+			scanf("%d %d", &x, &y);
+			arr[y][x] = 1;
+		}
 
-	memset(arr, 0, sizeof(arr));
-	
-	q.push(num);
-	arr[num] = 1;	//시작 지점을 재방문하지 않도록 하기 위해 1을 표시해줍니다.
-					//따라서 나중에 결과창에서 출력을 할 때, -1을 해준 값을 표시해주어야 합니다.(task1)
-	while (!q.empty()) {
-		int cur = q.front();
-		q.pop();
-		if (cur == 1) break;//도착지점에 도착했습니다! (정수 N을 1로 만들었습니다!)
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < m; ++j)
+				if (arr[i][j] == 1)
+				{
+					flood_fill(i, j);
+					++count;
+				}
 
-		if (cur % 3 == 0) {				//X가 3으로 나누어 떨어진다면
-			if (1 < num / 3 && arr[num / 3] == 0) {	//범위를 벗어나지 않으며, 방문하지 않은 항이라면
-				q.push(num / 3);		//3으로 나누는 연산을 실행합니다.(연산 1을 통해 vertex를 이동합니다.)
-				arr[num / 3] = arr[cur] + 1;
-			}
-		}
-		if (cur % 2 == 0) {
-			if (1 < num/2 && arr[num / 2] == 0) {	//범위를 벗어나지 않으며, 방문하지 않은 항이라면
-				q.push(num / 2);
-				arr[num / 2] = arr[cur] + 1;
-			}
-		}
-		if (0 <= num-1 && arr[num - 1] == 0) {		
-			q.push(num - 1);
-			arr[num - 1] = arr[cur] + 1;
-		}
+		printf("%d\n", count);
 	}
-	cout << arr[1]-1 << endl;		//task1
 	return 0;
 }
